861bb99f11b0cac350d9dbae5714598e543c343c
Joaos98 <33532434+Joaos98@users.noreply.github.com>
2017-11-30 16:37:57 -0200

    Mudei fonte dos nomes e coloquei em maiúsculo

--- src/tentativa1/GameOver.java
+++ src/tentativa1/GameOver.java
@@ -25,11 +25,11 @@ public abstract class GameOver {
         Image box = boxIcon.getImage();
         g.drawImage(box, (800-boxIcon.getIconWidth())/2, 200, null);
         String recordPontos = Float.toString(board.getPoints().getRecord()[0].getPontos());
-        String recordJogador = board.getPoints().getRecord()[0].getJogador();
+        String recordJogador = board.getPoints().getRecord()[0].getJogador().toUpperCase();
         String recordPontos2 = Float.toString(board.getPoints().getRecord()[1].getPontos());
-        String recordJogador2 = board.getPoints().getRecord()[1].getJogador();
+        String recordJogador2 = board.getPoints().getRecord()[1].getJogador().toUpperCase();
         String recordPontos3 = Float.toString(board.getPoints().getRecord()[2].getPontos());
-        String recordJogador3 = board.getPoints().getRecord()[2].getJogador();
+        String recordJogador3 = board.getPoints().getRecord()[2].getJogador().toUpperCase();
 
         g.setFont(new Font("Century Gothic", Font.PLAIN, 45));
         g.drawString(Float.toString(board.getPoints().getPoints()), 440, 256);
@@ -37,7 +37,7 @@ public abstract class GameOver {
         g.drawString(recordPontos, 370, 460);
         g.drawString(recordPontos2, 260, 460);
         g.drawString(recordPontos3, 490, 460);
-        g.setFont(new Font("Century Gothic", Font.PLAIN, 23));
+        g.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 23));
         g.drawString(recordJogador, 373, 350);        
         g.drawString(recordJogador2, 260, 350);        
         g.drawString(recordJogador3, 480, 350);
